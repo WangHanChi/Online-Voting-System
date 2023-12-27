ec07fb1e047e4eb58fd515f0c48349337d634dc6
WangHanChi <hank1122334455667788990@gmail.com>
2023-12-28 00:56:50 +0800

    Reorganize project structure
    Remove the CMakeLists.txt in the root directory, and rewrite the
    CMakeLists.txt in server/ and client/ ensure that can compile
    individually. Also rewrtie the auto-build.sh to adapt it.

--- script/test.sh
+++ script/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gnome-terminal -- bash -c "./server/server 8888; exit"
-gnome-terminal -- bash -c "./client/client 127.0.0.1 8888; exit"
+gnome-terminal -- bash -c "./server/build/server 8888; exit"
+gnome-terminal -- bash -c "./client/build/client 127.0.0.1 8888; exit"
