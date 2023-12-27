f5266f9fe2248bf3326189158c3134b2f6ab4fe3
WangHanChi <hank1122334455667788990@gmail.com>
2023-12-28 00:56:50 +0800

    Reorganize project structure
    Remove the CMakeLists.txt in the root directory, and rewrite the
    CMakeLists.txt in server/ and client/ ensure that can compile
    individually. Also rewrtie the auto-build.sh to adapt it.

--- auto-build.sh
+++ auto-build.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
+# Define color codes
 YELLOW='\e[33;01m'
 UNDERLINE='\e[4m'
 RESET='\e[0m'
 
-if [ -d "build" ]; then
-  echo "Removing existing build directory..."
-  rm -r build/
-fi
+# Function to remove existing build directory
+remove_build_directory() {
+  if [ -d "$1" ]; then
+    echo "Removing existing build directory: $1"
+    rm -r "$1"
+  fi
+}
 
-mkdir build && cd build
+# Build server
+remove_build_directory "server/build"
+mkdir -p server/build && cd server/build
+echo -e "${YELLOW}${UNDERLINE}Building server...${RESET}"
 cmake ..
 make
+cd ../..
+
+# Build client
+remove_build_directory "client/build"
+mkdir -p client/build && cd client/build
+echo -e "${YELLOW}${UNDERLINE}Building client...${RESET}"
+cmake ..
+make
+cd ../..
+
+
 echo -e ""
-echo -e "${YELLOW}${UNDERLINE}Now please enter <make test> to verify this system${RESET}"
+echo -e "${YELLOW}${UNDERLINE}Now please enter <./script/test.sh> to verify this system${RESET}"
 echo -e ""
--- common/src/unix_utils.c
+++ common/src/unix_utils.c
@@ -1,12 +1,13 @@
 #include "unix_utils.h"
-#include <errno.h>
 #include <assert.h>
+#include <errno.h>
 #include <sys/socket.h>
 
-ssize_t retry_send(int sock_fd, const void *buf, size_t len, int flags) {
+ssize_t retry_send(int sock_fd, const void *buf, size_t len, int flags)
+{
     ssize_t sent_len = 0;
-    while (sent_len != (ssize_t)len) {
-        const void *send_buf = (void*)((uintptr_t)buf + sent_len);
+    while (sent_len != (ssize_t) len) {
+        const void *send_buf = (void *) ((uintptr_t) buf + sent_len);
         ssize_t ret = send(sock_fd, send_buf, len - sent_len, flags);
         if (ret > 0)
             sent_len += ret;
@@ -22,10 +23,11 @@ ssize_t retry_send(int sock_fd, const void *buf, size_t len, int flags) {
     return sent_len;
 }
 
-ssize_t retry_recv(int sock_fd, void *buf, size_t len, int flags) {
+ssize_t retry_recv(int sock_fd, void *buf, size_t len, int flags)
+{
     ssize_t received_len = 0;
-    while (received_len != (ssize_t)len) {
-        void *recv_buf = (void*)((uintptr_t)buf + received_len);
+    while (received_len != (ssize_t) len) {
+        void *recv_buf = (void *) ((uintptr_t) buf + received_len);
         ssize_t ret = recv(sock_fd, recv_buf, len - received_len, flags);
         if (ret > 0)
             received_len += ret;
