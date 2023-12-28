26adb07607b1c9268fef69e84f1c32e0fd035291
jumha <e24076750@gs.ncku.edu.tw>
2023-12-28 18:37:02 +0800

    Add library to handle packet send/recv

---
+++ common/inc/proto_lib.h
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+#include "unix_utils.h"
+
+int recv_packet(int fd, int *type, int *tag, size_t *payload_len, void* *payload);
+int send_packet(int fd, int type, int tag, size_t payload_len, void *payload);
---
+++ common/src/proto_lib.c
@@ -0,0 +1,44 @@
+#include "proto_lib.h"
+#include "proto.h"
+#include "unix_utils.h"
+
+int recv_packet(int fd, int *type, int *tag, size_t *data_len, void* *data)
+{
+    int terminate = 0;
+    PktHdr_t header;
+    ssize_t ret = retry_recv(fd, (void *)&header, sizeof(header), 0);
+    if (ret == sizeof(header)) {
+        *type = header.type;
+        *tag = header.tag;
+        *data_len = header.payload_len;
+        *data = NULL;
+        if (header.payload_len) {
+            *data = (void *)malloc(header.payload_len);
+            if (*data) {
+                ret = retry_recv(fd, *data, header.payload_len, 0);
+                if (ret != (ssize_t)header.payload_len) // Should not happen
+                    abort();
+            }
+        }
+    } else {
+        terminate = 1;
+    }
+    return terminate;
+}
+
+int send_packet(int fd, int type, int tag, size_t data_len, void *data)
+{
+    int ret = 0;
+    PktHdr_t header;
+    header.type = type;
+    header.tag = tag;
+    header.payload_len = data_len;
+    ret = retry_send(fd, (void *)&header, sizeof(header), 0);
+    if (ret == sizeof(header)) {
+        ret = 0;
+        if (data_len && data) {
+            ret = retry_send(fd, (void *)&header, sizeof(header), 0);
+        }
+    }
+    return ret;
+}
